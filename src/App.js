ca0d7e1e109f7359f79929bf71651dd00488992b
Steven <steven.j.moe@gmail.com>
2018-12-14 16:20:20 +1100

    need to finish placeholder and style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import MarkdownPreviewer from './markdown-previewer';
 
+let marked = require("marked");
 class App extends Component {
   render() {
     return (
--- src/markdown-previewer.js
+++ src/markdown-previewer.js
@@ -1,33 +1,36 @@
 import React, { Component } from 'react';
 import './markdown-previewer.css';
 
+let marked = require("marked");
+marked.setOptions({breaks: true});
 
+//TODO -  list, blockquote, image, bolded text
+const placeholder = 
+"# This is a heading \n ## This is a subheading \n [This is an inline style link](www.google.com) \n This is inline code`<div></div>` \n and this is a code block \`\`\` function(param1, param2) \`\`\`"
 
 class MarkdownPreviewer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            userInput: "",
-
+            markdown: placeholder
         }
         this.handleChange = this.handleChange.bind(this);
-
     }
 
-    handleChange(i) {
+    handleChange(event) {
         this.setState({
-            userInput: i.target.value
+            markdown: event.target.value
         })
     }
 
     render() {
         return (
             <div>
-                <h1>Markdown Previewer</h1>
-                <textarea value={this.state.userInput} onChange={this.handleChange} id="editor"></textarea>
+                <p>Enter markdown</p>
+                <textarea value={this.state.markdown} onChange={this.handleChange} id="editor">OI</textarea>
                 <div id="preview">
-                    <h2>Preview</h2>
-                    {this.state.userInput}
+                    <h1>Markdown output</h1>
+                    <div dangerouslySetInnerHTML = {{__html: marked(this.state.markdown)}}></div>
                 </div>
             </div>
             
