0037af99638e57ced4efe51a8e8e0cbd0ed957d1
Steven <steven.j.moe@gmail.com>
2018-12-15 13:50:12 +1100

    mostly finished. still doesn't look very good

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import MarkdownPreviewer from './markdown-previewer';
+import {Helmet} from "react-helmet";
 
 let marked = require("marked");
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App container">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Markdown Previewer</title>
+
+        </Helmet>
         <MarkdownPreviewer />       
       </div>
     );
--- src/markdown-previewer.js
+++ src/markdown-previewer.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 import './markdown-previewer.css';
+import {Grid, Row, Col} from 'react-flexbox-grid'; 
 
 let marked = require("marked");
-marked.setOptions({breaks: true});
+marked.setOptions({
+    gfm: true,
+    breaks: true,
+});
+
+const renderer = new marked.Renderer();
+
+renderer.link = function(href, title, text)  {
+    return `<a target="_blank" href="${href}">${text}` + '</a>';
+}
 
 //TODO -  list, blockquote, image, bolded text
-const placeholder = 
-"# This is a heading \n ## This is a subheading \n [This is an inline style link](www.google.com) \n This is inline code`<div></div>` \n and this is a code block \`\`\` function(param1, param2) \`\`\`"
+const placeholder = "# This is a heading"
+                    +  "\n ## This is a subheading"
+                    + "\n [This](www.google.com) is a link"
+                    + "\n ` here is some inline code` "
+                    + "\n \`\`\` and this is a block of multiline code"
+                    + "\n another line"
+                    + "\n another line \`\`\`"
+                    + "\n * list item 1"
+                    + "\n * list item 2"
+                    + "\n > This is a blockquote!"
+                    + "\n Here is a cat:  \n![a cat picture](https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=150)"
+                    + "\n **and finally here is some bolded text**"
 
 class MarkdownPreviewer extends Component {
     constructor(props) {
@@ -25,14 +45,24 @@ class MarkdownPreviewer extends Component {
 
     render() {
         return (
-            <div>
-                <p>Enter markdown</p>
-                <textarea value={this.state.markdown} onChange={this.handleChange} id="editor">OI</textarea>
-                <div id="preview">
-                    <h1>Markdown output</h1>
-                    <div dangerouslySetInnerHTML = {{__html: marked(this.state.markdown)}}></div>
-                </div>
-            </div>
+            <Grid fluid>
+                <h1 id="title">Markdown Previewer</h1>
+                <Row>
+                    <Col xs={12} md={6}>
+                        <h3>Input: </h3>
+                        <div id="editor">
+                            <textarea value={this.state.markdown} onChange={this.handleChange} />
+                        </div>
+                    </Col>
+                    <Col xs={12} md={6} >
+                        <h3>Output: </h3>
+                        <div id="preview">
+                            <div dangerouslySetInnerHTML = {{__html: marked(this.state.markdown)}}></div>
+                        </div>
+                    </Col>
+                </Row>
+                
+            </Grid>
             
         )
     }
