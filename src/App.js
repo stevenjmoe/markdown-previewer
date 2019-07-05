ffc2d362b60d923d3c1475912998ebc6548664c8
unknown <steven.j.moe@gmail.com>
2019-07-05 17:54:18 +1000

    reformat -needs colours

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css';
+import './style/App.css';
 import MarkdownPreviewer from './markdown-previewer';
 import {Helmet} from "react-helmet";
 
@@ -8,7 +7,7 @@ let marked = require("marked");
 class App extends Component {
   render() {
     return (
-      <div className="App container">
+      <div className="App">
         <Helmet>
           <meta charSet="utf-8" />
           <title>Markdown Previewer</title>
